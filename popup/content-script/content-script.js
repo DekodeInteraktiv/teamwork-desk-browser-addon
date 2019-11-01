f62ddda693e6ce3165728a8a281ab1a78b91029f
rmkristiansen <roy@dekode.no>
2019-11-01 11:30:07 +0100

    Moved project to git

---
+++ popup/content-script/content-script.js
@@ -0,0 +1,67 @@
+
+(function() {
+    /*
+     * Check and set a global guard variable.
+     * If this content script is injected into the same page again,
+     * it will do nothing next time.
+     */
+    if (window.hasRun) {
+      return;
+    }
+    window.hasRun = true;
+
+    function sendBrowserContent(){
+      var data = getTeamwworkDataFromSingleTicketPage(); 
+      var content = formatTeamWorkData(data);
+      return content; 
+    }
+
+    function formatTeamWorkData(data){
+      return data.ticketSubject + ' (' + data.ticketId + ') ' + data.ticketCustomerFullname; 
+    }
+
+    function getTeamwworkDataFromSingleTicketPage(){
+
+      var ticketId = document.getElementsByClassName("ticket-id")[0].innerHTML;
+      var ticketCustomerFullname = document.getElementsByClassName("customer-fullname")[0].innerHTML;
+      var ticketSubject = document.getElementsByClassName("title__subject")[0].innerHTML;
+
+      var data = {
+        ticketId: ticketId.trim(),
+        ticketCustomerFullname: ticketCustomerFullname.trim(),
+        ticketSubject: ticketSubject.trim(),
+      }
+
+      return data; 
+
+    }
+
+
+    function sendContentToPopup(){
+
+        var extensionId = null;
+        var message = {
+            command: 'SEND_BROWSER_CONTENT',
+            data: sendBrowserContent()
+        }
+        var options = {}
+
+        browser.runtime.sendMessage(extensionId, message, options)
+        .catch((error) => {
+          console.log(error);
+        });
+  
+    }
+  
+    /**
+     * Listen for messages from the background script.
+     * Call "beastify()" or "reset()".
+    */
+
+    browser.runtime.onMessage.addListener((message) => {
+        if (message.command === "POPUP_FETCH_CONTENT") {
+          sendContentToPopup();
+        }
+    });
+  
+  })();
---
+++ popup/popup.js
@@ -0,0 +1,60 @@
+
+
+function writeToElement(content){
+  document.getElementById('content').innerHTML = content;
+}
+
+function fetchContent(tabs){
+  // Send message to the current open tab. 
+  browser.tabs.sendMessage(tabs[0].id, {
+    command: "POPUP_FETCH_CONTENT"
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+}
+
+function listenForClicks(){
+
+  // Register click event. 
+  document.getElementById('fetch_button').addEventListener("click", (event) => {
+
+    // Stop button default
+    event.preventDefault(); 
+
+    // Get all active browser tabs.
+    browser.tabs.query({active: true, currentWindow: true})
+    .then((tabs) => {
+      // Send the list of tabs to fetch.
+      fetchContent(tabs);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+    
+  });
+
+}
+
+// Register listner to recive messages from content script. 
+browser.runtime.onMessage.addListener((message) => {
+
+  if (message.command === "SEND_BROWSER_CONTENT") {
+    writeToElement(message.data);
+  }
+
+});
+
+// Add polyfill.
+browser.tabs.executeScript({file: "/popup/polyfill/browser-polyfill.js"})
+.catch((error) => {
+  console.log(error);
+});
+
+// Add browser content script
+browser.tabs.executeScript({file: "/popup/content-script/content-script.js"})
+.then(listenForClicks())
+.catch((error) => {
+  console.log(error);
+});
